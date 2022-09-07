b97b72078675bfff7a313992c3a44663de29783c
leejpsd <wndvy7690@nate.com>
2022-09-08 01:19:16 +0900

    m

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,75 +1,83 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled from 'styled-components';
 import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
-import Category, { postCategory } from "../../redux/modules/category";
-import { getDateTodo } from "../../redux/modules/dateTodos";
-import { RootState } from "../../redux/config/configStore";
-import { Icategory } from "../../api";
+import Category, { postCategory } from '../../redux/modules/category';
+import { getDateTodo } from '../../redux/modules/dateTodos';
+import { RootState } from '../../redux/config/configStore';
+
 
 const TodoList = () => {
+  interface Icategory {
+    categoryId:number;
+    categoryName:string;
+    todoList:[];
+  }
+
   const dispatch = useDispatch();
-  const date = useSelector((state: RootState) => state.updateDate.date);
-  const dateTodos: Array<Icategory> = useSelector(
-    (state: RootState) => state.dateTodos.dateTodos
-  );
+  const date= useSelector((state:RootState) => state.updateDate.date);
+  const dateTodos:Array<Icategory>= useSelector((state:RootState) => state.dateTodos.dateTodos);
+  
+  const [btnShow,setBtnShow] = useState(false)
+  const [btn2Show,setBtn2Show] = useState(false)
+  const [btn3Show,setBtn3Show] = useState(false)
+  const [btn4Show,setBtn4Show] = useState(false)
 
-  const [btnShow, setBtnShow] = useState(false);
-  const [btn2Show, setBtn2Show] = useState(false);
-  const [btn3Show, setBtn3Show] = useState(false);
-  const [btn4Show, setBtn4Show] = useState(false);
+  const [category,setCategory] = useState('')
 
-  const [category, setCategory] = useState("");
-
-  console.log("선택날짜데이터", dateTodos);
+  console.log('선택날짜데이터',dateTodos)
 
   const onSubmitHandler = () => {
-    dispatch(postCategory({ categoryName: "카테고리", selectDate: date }));
-    window.location.reload();
+    dispatch(postCategory(
+      {categoryName:'카테고리',
+      selectDate:date}
+    ))
+    window.location.reload()
   };
 
-  // 선택되는 날짜 받아와서 정보불러오기 (기본값 오늘날짜)
+ // 선택되는 날짜 받아와서 정보불러오기 (기본값 오늘날짜)
 
-  useEffect(() => {
-    dispatch(getDateTodo(moment(date).format("YYYY-MM-DD")));
-  }, [date]);
+useEffect(() => {
+  dispatch(getDateTodo(moment(date).format("YYYY-MM-DD")))
+}, [date]);
+
 
   return (
     <>
-      <AddCategory>
-        <BtnGroup>
-          <div onClick={onSubmitHandler}></div>
-        </BtnGroup>
-      </AddCategory>
+    <AddCategory>
+      <BtnGroup>
+        <div onClick={onSubmitHandler}></div>
+      </BtnGroup>
+    </AddCategory>
 
-      <TodoListBox>
-        {dateTodos?.map((list: any) => (
-          <CategoryBox key={list.categoryId}>
-            <CategoryTitle>
-              <div></div>
-              <p>{list.categoryName}</p>
-            </CategoryTitle>
-            <CategoryList>
-              {list.todoList.map((item: any) => (
-                <CategoryListBox key={item.todoId}>
-                  <p>{item.content}</p>
-                  <div></div>
-                </CategoryListBox>
-              ))}
-            </CategoryList>
-          </CategoryBox>
-        ))}
-      </TodoListBox>
+    <TodoListBox>
+      {dateTodos?.map((list:any)=> 
+      <CategoryBox key={list.categoryId}>
+        <CategoryTitle >
+          <div></div>
+          <p>{list.categoryName}</p>
+        </CategoryTitle>
+        <CategoryList>
+          {list.todoList.map((item:any)=>
+            <CategoryListBox key={item.todoId}>
+            <p>{item.content}</p>
+            <div></div>
+          </CategoryListBox>)}
+        </CategoryList>
+      </CategoryBox>)}
+    </TodoListBox>
     </>
+    
+    
   );
 };
 
 export default TodoList;
 
-const AddCategory = styled.div``;
+const AddCategory = styled.div``
 
 const BtnGroup = styled.div`
-  display: flex;
+display:flex;
   div {
     width: 40px;
     height: 40px;
@@ -87,56 +95,57 @@ const BtnGroup = styled.div`
       background: green;
     }
   }
-`;
+`
 
 const TodoListBox = styled.section`
-  width: 100%;
-  height: 100%;
+  width:100%;
+  height:100%;
   border: solid red 3px;
   padding: 10px 0;
-`;
+`
 
-const CategoryBox = styled.div``;
+const CategoryBox = styled.div`
+`
 
 const CategoryTitle = styled.div`
-  display: flex;
+  display:flex;
   input {
     margin-left: 5px;
   }
-  div {
+  div{
     width: 40px;
     height: 40px;
     border-radius: 50%;
-    background: #c7b5ef;
+    background: #C7B5EF;
   }
-`;
+`
 
 const CategoryList = styled.div`
-  list-style: none;
-`;
+  list-style:none;
+`
 const CategoryListBox = styled.div`
-  margin-left: 35px;
-  width: 379px;
-  height: 40px;
-  border-radius: 20px;
-  background: #c7b5ef;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 5px 0 20px;
-  margin-top: 10px;
-  p {
-    font-family: "Inter";
-    font-style: normal;
-    font-size: 15px;
-    line-height: 30px;
-    letter-spacing: 0.15em;
-    color: #000;
-  }
-  div {
-    width: 47px;
-    height: 30px;
-    background: rgba(244, 244, 244, 0.8);
+    margin-left: 35px;
+    width: 379px;
+    height: 40px;
     border-radius: 20px;
-  }
-`;
+    background: #C7B5EF;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 5px 0 20px;
+    margin-top: 10px;
+      p {
+        font-family: 'Inter';
+font-style: normal;
+font-size: 15px;
+line-height: 30px;
+letter-spacing: 0.15em;
+color: #000;
+    }
+      div {
+      width: 47px;
+      height: 30px;
+      background: rgba(244, 244, 244, 0.8);
+      border-radius: 20px;
+    }
+  `
--- src/redux/modules/category.tsx
+++ src/redux/modules/category.tsx
@@ -2,51 +2,52 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export interface category {
-  content: any;
-  categoryName: any;
-  headers: string;
+  content : any,
+  categoryName : any,
+  headers: string
 }
 
 //일자별 목록 조회
 
-export const postCategory: any = createAsyncThunk(
+export const postCategory : any = createAsyncThunk(
   "category/postCategory",
-  async (payload: any, thunkAPI) => {
-    console.log("axios", payload);
+  async (payload:any, thunkAPI) => {
+    console.log('axios',payload)
     try {
-      const data = await axios.post(
-        "http://54.180.79.105/api/v1/todoCategories",
-        {
-          categoryName: payload.categoryName,
-          selectDate: payload.selectDate,
+      const data = await axios.post("http://54.180.79.105/api/v1/todoCategories",{
+        categoryName: payload.categoryName,
+        selectDate: payload.selectDate,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrbXNAZ21haWwuY29tIiwiaXNzIjoiaGFuZ2hhZTVfc2VhdHVkeSIsImV4cCI6MTY2MjU1MDQzMX0.pl0AJBZaI_IJHqEfvfHKmlgaVqahoXHa1b4sGv6fSXQ'
         },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization:
-              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrbXNAZ21haWwuY29tIiwiaXNzIjoiaGFuZ2hhZTVfc2VhdHVkeSIsImV4cCI6MTY2MjU1MDQzMX0.pl0AJBZaI_IJHqEfvfHKmlgaVqahoXHa1b4sGv6fSXQ",
-          },
-        }
-      );
-      return thunkAPI.fulfillWithValue(data.data.data);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
+      })
+    return thunkAPI.fulfillWithValue(data.data.data);
+  } catch (error) {
+  return thunkAPI.rejectWithValue(error);
+}
   }
 );
+
 
 const initialState = {
   category: [],
 };
 
+
 export const categorySlice = createSlice({
   name: "category",
   initialState,
-  reducers: {},
-  extraReducers: {
-    // [postCategory.fulfilled]: (state, action) => {
-    //   state.category.concat(action.payload)}
+  reducers: {
   },
-});
+  extraReducers: {
+      // [postCategory.fulfilled]: (state, action) => {
+      //   state.category.concat(action.payload)}
+      }
+    } 
+);
 
-export default categorySlice.reducer;
+
+export default categorySlice.reducer; 
