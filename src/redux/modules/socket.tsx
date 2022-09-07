3a6021595c8156a8f19c3edd7006e20f96cd3232
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-09-07 20:28:13 +0900

    Create socket.tsx

---
+++ src/redux/modules/socket.tsx
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const accessToken =
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJobGltOTAyMkBnbWFpbC5jb20iLCJpc3MiOiJoYW5naGFlNV9zZWF0dWR5IiwiZXhwIjoxNjYyNjA2MjEwfQ.IHaY6U-3-UQJzwggQtCzVVv6Dh45WH8VNm5fZShQpzo";
+
+const BASE_URL = "http://43.200.115.252";
+const roomId = `4e66fa90-e834-4b51-b17d-c222979cc04c`;
+
+export const __getChatroom: any = createAsyncThunk(
+  "get/chatroom",
+  async (payload: any, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/v1/chat/room/${roomId}`,
+        {
+          headers: {
+            contentType: "application/json",
+            authorization: accessToken,
+          },
+        }
+      );
+      console.log(response);
+      return response.data.data.roomId;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  chat: [],
+};
+
+export const preChatSlice = createSlice({
+  name: "chat",
+  initialState,
+  reducers: {
+    addMessage: (state, action) => {
+      state.chat.concat(action.payload);
+    },
+  },
+  extraReducers: {
+    [__getChatroom.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      // state.roomId = action.payload;
+    },
+  },
+});
+
+export const { addMessage } = preChatSlice.actions;
+export default preChatSlice.reducer;
