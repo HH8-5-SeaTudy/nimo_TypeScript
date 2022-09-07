60e97e42d7c5e4518334af423daa705cf2be46f2
peppermintt0504 <73277351+peppermintt0504@users.noreply.github.com>
2022-09-07 20:27:07 +0900

    update modules

--- src/redux/config/configStore.tsx
+++ src/redux/config/configStore.tsx
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todos from "../modules/todos";
+import allTodos from "../modules/allTodos";
+import dateTodos from '../modules/dateTodos';
 import timer from "../modules/timer";
+import category from '../modules/category';
+import updateDate from '../modules/searchDate';
 
 const store = configureStore({
   reducer: {
-    todos,
+    updateDate,
+    allTodos,
+    dateTodos,
+    category,
     timer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+    
+  
+  } 
 });
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 export default store;
