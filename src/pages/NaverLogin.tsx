720f09ff2a15df666116cb28b1c0626bd5587362
eunyechan <glqgkq384@naver.com>
2022-09-21 13:18:16 +0900

    Fix:달력 수정 merge

--- src/pages/NaverLogin.tsx
+++ src/pages/NaverLogin.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import { setCookie } from "../components/social/Cookie";
-import { useNavigate } from 'react-router-dom';
-import { useAppDispatch} from "../components/hooks/reduxHooks";
-import { updateUser } from '../redux/modules/userData';
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../components/hooks/reduxHooks";
+import { updateUser } from "../redux/modules/userData";
 
 const NaverLogin = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const code = new URL(window.location.href).searchParams.get("code");
   const state = new URL(window.location.href).searchParams.get("state");
-  console.log(code)
-  console.log(state)
+  console.log(code);
+  console.log(state);
   const BASE_URL = process.env.REACT_APP_BASE_URL;
   useEffect(() => {
     const naver = async () => {
@@ -21,13 +21,12 @@ const NaverLogin = () => {
           `${BASE_URL}/api/v1/members/naverLogin?code=${code}&state=${state}`
         )
         .then((res) => {
-        setCookie("token", res.headers.authorization);
-        dispatch(updateUser(res.data.data));
-      })
+          setCookie("token", res.headers.authorization);
+          dispatch(updateUser(res.data.data));
+        })
         .then(() => {
           navigate("/home");
-        })
-
+        });
     };
     if (code) {
       naver();
