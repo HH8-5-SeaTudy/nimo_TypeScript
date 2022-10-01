6e098e4609cc57d9e9dd9b9bae64aa4f82cd17d5
leejpsd <wndvy7690@nate.com>
2022-10-01 12:31:18 +0900

    master

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 function PrivateRoute({ token, component: Component }) {
   return token 
   ? (Component) 
-  : (<Navigate to="/login" {...alert("로그인이 필요한 페이지입니다.")} />);
+  : (<Navigate to="/" {...alert("로그인이 필요한 페이지입니다.")} />);
 }
 
 export default PrivateRoute;
