9aef5e784cfdc81c5ce3f14c550d1675c18f3746
jungpyo lee <101346918+leejpsd@users.noreply.github.com>
2022-10-03 15:25:41 +0900

    디데이 버그 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -18,35 +18,57 @@ import logo from "../../assets/logo/seatudyLogo.png";
 import CalendarVer2 from "../calendar/CalendarVer2";
 import { __getDayMyRank, __getWeekMyRank } from "../../redux/modules/rank";
 import Grid from "../../elements/Grid";
-import { __getUserProfile } from '../../redux/modules/userData';
-import fishImages from '../fish/FishImages';
-import { getCookie } from '../social/Cookie';
-import { __getDday } from '../../redux/modules/dday';
+
+import { __getUserProfile } from "../../redux/modules/userData";
+import fishImages from "../fish/FishImages";
+import { getCookie } from "../social/Cookie";
+import { __getDday } from "../../redux/modules/dday";
+import axios from 'axios';
 
 const Header = () => {
-
   const token: string = getCookie("token") as string;
-
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const today = new Date();
   const year = today.getFullYear();
-  const month = ('0' + (today.getMonth() + 1)).slice(-2);
-  const day = ('0' + today.getDate()).slice(-2);
-  const dateString = year + '-' + month  + '-' + day;
-
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const day = ("0" + today.getDate()).slice(-2);
+  const dateString = year + "-" + month + "-" + day;
+  const [todayDday,setTodayDday] = useState<any>([])
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const time = useAppSelector((state) => state.timer);
   const dayMyRank = useAppSelector((state) => state.rank.dayMyRank.myRank);
   const weekMyRank = useAppSelector((state) => state.rank.WeekMyRank.myRank);
-  const Dday = useAppSelector((state)=>state.dday.DdayData)
+  const Dday = useAppSelector((state) => state.dday.DdayData);
   const userData = useAppSelector((state) => state.userData.userProfile);
   const fishPoint = fishImages.map((data) => data.point);
   const userPoint = userData.point;
   const nextFishPoint = fishPoint.filter((x) => x > userPoint)[0];
   const nextPercent = (userPoint / nextFishPoint) * 100;
   const nextFishImg = fishImages.find((x) => x.point === nextFishPoint)?.image;
-  const NextDday = Dday.filter((x)=>x.targetDay >= dateString).sort((a,b)=>b.dday - a.dday)[0]
   
+  const TodayStudyData = async () => {
+    return await axios
+    .get(`${BASE_URL}/api/v1/ddays/dates?selectDate=${dateString}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      })
+    .then((res) => {
+      setTodayDday(res.data.data)
+    })
+  };
+
+  const NextDday = todayDday.filter((x:any) => x.targetDay >= dateString).sort(
+    (a:any, b:any) => b.dday - a.dday
+  )[0];
+
+useEffect (()=>{
+  TodayStudyData()
+},[Dday])
+console.log(NextDday)
+
   const [asmrShow, setAsmrShow] = useState(false);
   const [showTodo, setShowTodo] = useState(false);
   const [hh, mm, ss] = String(time.dayStudyTime)
@@ -64,7 +86,6 @@ const Header = () => {
     dispatch(__getDayMyRank());
     dispatch(__getWeekMyRank());
     dispatch(__getUserProfile());
-    dispatch(__getDday(dateString));
     return () => {
       dispatch(__getCheckOutTimer());
     };
@@ -112,6 +133,7 @@ const Header = () => {
           <HeaderLogo src={logo} onClick={() => navigate("/home")} />
         </HeaderLogoContainer>
         {/* 소라버튼 */}
+
           <AsmrBtn>
             <OnAsmr src={shell} onClick={() => setAsmrShow(!asmrShow)} />
             {asmrShow && <Asmr />}
@@ -150,10 +172,9 @@ const Header = () => {
               <br />
               {NextDday?.dday === 0 ? "Day" : String(NextDday?.dday).slice(1)}
             </DdayTitle>
-            <DdayContent>{NextDday?.title}</DdayContent>
-
+            <DdayContent>{NextDday?.targetDay}<br/>{NextDday?.title}</DdayContent>
           </DdayBtn>
-          }
+        )}
 
         <HeaderTimerContainer>
           <HeaderTimer>
@@ -186,7 +207,7 @@ const Layer = styled.div`
 `;
 
 const HeaderContainer = styled.div`
-  position:relative;
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -194,6 +215,7 @@ const HeaderContainer = styled.div`
   height: 65px;
   padding: 0px 55px;
   height: 10vh;
+
   box-shadow: 1px 1px 3px 1px #dadce0;
   background: #ff9100;
 `;
@@ -217,7 +239,6 @@ const HeaderTimer = styled.span`
   color: #fff;
   font-size: 50px;
 `;
-
 
 const AsmrBtn = styled.button`
   position: absolute;
@@ -254,7 +275,7 @@ const CalendarBtn = styled.button`
   }
 `;
 
-const FishBtn =styled.button`
+const FishBtn = styled.button`
   position: absolute;
   left: 40%;
   width: 80px;
@@ -268,11 +289,11 @@ const FishBtn =styled.button`
     background-color: rgba(0, 0, 0, 0.5);
   }
   p {
-    color : black;
+    color: black;
     font-weight: 700;
   }
-`
-const RankBtn =styled.button`
+`;
+const RankBtn = styled.button`
   position: absolute;
   left: 50%;
   width: 60px;
@@ -287,7 +308,7 @@ const RankBtn =styled.button`
   }
   p {
     position: absolute;
-    color : black;
+    color: black;
     font-weight: 700;
     width: 80px;
     left: -8px;
@@ -323,7 +344,7 @@ const DdayBtn = styled.div`
       display: flex;
     }
   }
-  `
+`;
 
 const DdayTitle = styled.div`
   width: 100%;
@@ -332,61 +353,30 @@ const DdayTitle = styled.div`
   line-height: 17px;
   font-weight: 700;
   border: solid black 2px;
-  background-color: #7DCCFF;
-  display:flex;
+  background-color: #7dccff;
+  display: flex;
   justify-content: center;
   align-items:center;
   border-radius:6px;
 `
 const DdayContent =styled.p`
 position:absolute;
-width: 150%;
+width: 140%;
 border-radius: 6px;
+
   font-size: 14px;
-  z-index:3;
+  z-index: 3;
   line-height: 15px;
-  padding:2px;
-  background-color:#B2E2FF;
+  padding: 3px 5px;
+  background-color: #b2e2ff;
   display: none;
+  text-align:center;
   border: solid white 2px;
-`
+`;
 
-const Rank = styled.div`
-border: solid red 1px;
-p{
-border: solid red 1px;
-}
-
-`
 const Calendar = styled.img`
   width: 100%;
   height: 100%;
-`;
-
-const RankContainer = styled.div`
-position: absolute;
-left: 60%;
-border: solid red 1px;
-  width: 20%;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  border-radius: 10px;
-`;
-
-const Nickname = styled.span`
-  font-size: 1.2em;
-  color: black;
-`;
-
-const DayRank = styled.span`
-  font-size: 1.2em;
-  color: black;
-`;
-
-const WeekRank = styled.span`
-  font-size: 1.2em;
-  color: black;
 `;
 
 export default Header;
