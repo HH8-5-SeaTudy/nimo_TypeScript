6c910bf2cc34afd0304b852f65fe6aae57319fd7
leejpsd <wndvy7690@nate.com>
2022-10-05 18:00:26 +0900

    스탑워치컴포넌트

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -20,6 +20,7 @@ import fishImages from "../fish/FishImages";
 import { getCookie } from "../social/Cookie";
 import axios from "axios";
 import Asmr from "../asmr/Asmr";
+import StopWatch from '../stopwatch/StopWatch';
 
 const Header = () => {
   const token: string = getCookie("token") as string;
@@ -33,7 +34,6 @@ const Header = () => {
   const [todayDday, setTodayDday] = useState<any>([]);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const time = useAppSelector((state) => state.timer);
   const dayMyRank = useAppSelector((state) => state.rank.dayMyRank.myRank);
   const weekMyRank = useAppSelector((state) => state.rank.WeekMyRank.myRank);
   const Dday = useAppSelector((state) => state.dday.DdayData);
@@ -46,10 +46,16 @@ const Header = () => {
   const myPoint = userPoint - prevFishPoint;
   const nextPercent = (myPoint / totalFishPoint) * 100;
   const nextFishImg = fishImages.find((x) => x.point === nextFishPoint)?.image;
-
+  const [showTodo, setShowTodo] = useState(false);
   const [playing, setPlaying] = useState(false);
 
-  const TodayStudyData = async () => {
+  const roomId1 = process.env.REACT_APP_ROOMID1;
+  const roomId2 = process.env.REACT_APP_ROOMID2;
+  const roomId3 = process.env.REACT_APP_ROOMID3;
+  const roomId4 = process.env.REACT_APP_ROOMID4;
+  const roomId5 = process.env.REACT_APP_ROOMID5;
+
+  const TodayDdayData = async () => {
     return await axios
       .get(`${BASE_URL}/api/v1/ddays/dates?selectDate=${dateString}`, {
         headers: {
@@ -67,29 +73,12 @@ const Header = () => {
 
   useEffect(() => {
     if( token !== undefined) {
-      TodayStudyData();
+      TodayDdayData();
     }
-    
   }, [Dday]);
-
-  const [showTodo, setShowTodo] = useState(false);
-  const [hh, mm, ss] = String(time.dayStudyTime)
-    .split(":")
-    .map((v) => +v);
-
-  const [timeSS, setTimeSS] = useState<number>(0);
-  const [timeMM, setTimeMM] = useState<number>(0);
-  const [timeHH, setTimeHH] = useState<number>(0);
-
-  const roomId1 = process.env.REACT_APP_ROOMID1;
-  const roomId2 = process.env.REACT_APP_ROOMID2;
-  const roomId3 = process.env.REACT_APP_ROOMID3;
-  const roomId4 = process.env.REACT_APP_ROOMID4;
-  const roomId5 = process.env.REACT_APP_ROOMID5;
 
   useEffect(() => {
     if( token !== undefined) {
-          dispatch(__getUserinquire());
     dispatch(__getDayMyRank());
     dispatch(__getWeekMyRank());
     dispatch(__getUserProfile());
@@ -99,33 +88,6 @@ const Header = () => {
     }
   }, []);
 
-  useEffect(() => {
-    setTimeSS(ss);
-    setTimeMM(mm);
-    setTimeHH(hh);
-  }, [time]);
-
-  useEffect(() => {
-    let interval: any = null;
-    if (time.isStudy) {
-      interval = setInterval(() => {
-        setTimeSS((ss) => ss + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-    if (timeSS % 60 === 0 && timeSS !== 0) {
-      setTimeMM((mm) => mm + 1);
-    }
-
-    return () => clearInterval(interval);
-  }, [JSON.stringify(time), timeSS]);
-
-  useEffect(() => {
-    if (timeMM % 60 === 0 && timeMM !== 0) {
-      setTimeHH((hh) => hh + 1);
-    }
-  }, [timeMM]);
 
   if (window.location.pathname === "/login") return null;
   if (window.location.pathname === "/kakaoLogin") return null;
@@ -247,39 +209,15 @@ const Header = () => {
           </DdayBtn>
         )}
         <HeaderTimerContainer>
-          <HeaderTimer>
-            <Layer>
-              <Link to="/statistics" style={{ textDecoration: "none" }}>
-                <span>{("0" + Math.floor(timeHH % 24)).slice(-2)}:</span>
-                <span>
-                  {timeMM === undefined
-                    ? "00"
-                    : ("0" + Math.floor(timeMM % 60)).slice(-2)}
-                  :
-                </span>
-                <span>
-                  {timeSS === undefined
-                    ? "00"
-                    : ("0" + Math.floor(timeSS % 60)).slice(-2)}
-                </span>
-              </Link>
-            </Layer>
+          <HeaderTimer onClick={() => navigate("/statistics")}>
+            <StopWatch/>
           </HeaderTimer>
         </HeaderTimerContainer>
       </HeaderContainer>
     </>
   );
 };
-const Layer = styled.div`
-  span {
-    color: white;
-  }
-`;
-const CalendarLayer =styled.div`
-background-color: white;
-  width: 100vw;
-  height: 90vw;
-`
+
 const HeaderContainer = styled.div`
   position: relative;
   display: flex;
@@ -312,6 +250,7 @@ const HeaderTimerContainer = styled.div`
 const HeaderTimer = styled.span`
   color: #fff;
   font-size: 50px;
+  cursor: pointer;
 `;
 
 const AsmrBtn = styled.button`
